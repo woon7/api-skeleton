4b8074602560b1c85d02129797ad180c8874386d
woon7 <yongwoon.kim@brique.co.kr>
2020-06-09 17:20:09 +0900

    Mod deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,13 @@
 set USER_NAME=brique
-set JAR_PATH=C:\Users\%USER_NAME%\.m2\repository\com\brique\admc-api\0.0.1-SNAPSHOT
-set SOURCE_PATH=C:\admc\admc-api
+set API_VERSION=0.0.1-SNAPSHOT
+set JAR_PATH=C:\Users\%USER_NAME%\.m2\repository\com\brique\admc-api\%API_VERSION%
+set EXEC_JAR=admc-api-%API_VERSION%.jar
+set SRC_PATH=C:\admc\admc-api
 set HOST_URL=administrator@192.168.0.37
 set TARGET_PATH=C:\admc\admc-api
-set HOST_PASSWORD=Brique0901#$
-set EXEC_JAR=admc-api-*.jar
+set HOST_PW=Brique0901#$
 
 call mvnw install -DskipTests
-call pscp -pw %HOST_PASSWORD% -P 22 %JAR_PATH%\%EXEC_JAR% %HOST_URL%:%TARGET_PATH%
-call pscp -pw %HOST_PASSWORD% -P 22 -r %SOURCE_PATH%\config %HOST_URL%:%TARGET_PATH%
+call pscp -pw %HOST_PW% -P 22 %JAR_PATH%\%EXEC_JAR% %HOST_URL%:%TARGET_PATH%
+call pscp -pw %HOST_PW% -P 22 -r %SRC_PATH%\config %HOST_URL%:%TARGET_PATH%
+call plink -ssh %HOST_URL% -pw %HOST_PW% %TARGET_PATH%\start-docker.bat
