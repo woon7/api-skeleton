6c6565978ebdc023c833e590e3e622e3ced2eccb
Yongwoon Kim <righteous0319@daum.net>
2020-06-09 10:24:10 +0900

    Mod host url

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,11 @@
-set JAR_PATH=C:\Users\brique\.m2\repository\com\brique\admc-api\0.0.1-SNAPSHOT
+set USER_NAME=Enter
+set JAR_PATH=C:\Users\%USER_NAME%\.m2\repository\com\brique\admc-api\0.0.1-SNAPSHOT
 set SOURCE_PATH=C:\admc\admc-api
-set TARGET_PATH=administrator@192.168.0.37:C:\admc\admc-api
+set HOST_URL=administrator@office.brique.kr
+set TARGET_PATH=C:\admc\admc-api
 set HOST_PASSWORD=Brique0901#$
+set EXEC_JAR=admc-api-*.jar
 
 call mvnw install -DskipTests
-call pscp -pw %HOST_PASSWORD% %JAR_PATH%\admc-api-*.jar %TARGET_PATH%
-call pscp -pw %HOST_PASSWORD% -r %SOURCE_PATH%\config %TARGET_PATH%
+call pscp -pw %HOST_PASSWORD% -P 3322 %JAR_PATH%\%EXEC_JAR% %HOST_URL%:%TARGET_PATH%
+call pscp -pw %HOST_PASSWORD% -P 3322 -r %SOURCE_PATH%\config %HOST_URL%:%TARGET_PATH%
